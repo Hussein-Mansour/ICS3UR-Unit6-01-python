738d8d83531843ba38f6ef38b757a80d0ab2fc32
Hussein-Mansour <74609841+Hussein-Mansour@users.noreply.github.com>
2021-06-07 18:43:13 -0400

    Update random_number_average.py

--- random_number_average.py
+++ random_number_average.py
@@ -10,24 +10,29 @@ import random
 
 
 def main():
-    # this function uses an array
-    random_number = []
-    # start
-    print("Starting ...")
-    # input
-    for loop_counter in range(0, 10):
-        random_ = random.randint(1, 100)
-        random_number.append(random_)
-        # output
-        print(
-            "\nThe random number is: {0}"
-            .format(random_number[loop_counter]), end="")
-    # process
-    average = sum(random_number) / 10
-    # output
-    print("\n\nThe average is {0}".format(average))
-    print("\nDone.")
+   # this function uses an array
+   random_number = []
+   sum_num = 0
+   # start
+   print("Starting ...")
+   # input
+   for loop_counter in range(0, 10):
+       random_ = random.randint(1, 100)
+       random_number.append(random_)
+       # output
+       print(
+           "\nThe random number is: {0}"
+           .format(random_number[loop_counter]), end="")
+   # process
+   for counter in random_number:
+     sum_num = sum_num + counter
+
+   average = sum_num / len(random_number)
+
+   # output
+   print("\n\nThe average is {0}".format(average))
+   print("\nDone.")
 
 
 if __name__ == "__main__":
-    main()
+   main()
